b58fd168850c80dd05837a49fe36e7986d13e715
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2024-11-20 19:38:29 +0100

    chore: capture link clicks

--- src/app/book/[id]/route.ts
+++ src/app/book/[id]/route.ts
@@ -6,5 +6,23 @@ export async function GET(
 ) {
   const productId = (await params).id;
   const url = `http://www.amazon.com/dp/${productId}/ref=nosim?tag=goodreadsbotr-20`;
+
+  fetch("https://us.i.posthog.com/capture/", {
+    method: "POST",
+    cache: "no-cache",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      api_key: process.env.NEXT_PUBLIC_POSTHOG_KEY!,
+      distinct_id: "goodread_bots_reloaded",
+      event: "amazon_link_click",
+      properties: {
+        prodcut_id: productId,
+        product_url: url,
+      },
+    }),
+  });
+
   return redirect(url);
 }
